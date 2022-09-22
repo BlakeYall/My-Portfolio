8cc4f2625e0ff50be4f715d9d439f640760b5026
BlakeYall <blakesvsc@gmail.com>
2022-09-22 19:53:11 +0200

    Update HTML, JS and CSS

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const tablinks = document.getElementSByClassName('tab-links');
-const tabcontents = document.getElementSByClassName('tab-contents');
+/* eslint-disable linebreak-style */
+const tablinks = document.getElementSByClassName('.tab-links');
+const tabcontents = document.getElementSByClassName('.tab-contents');
+// eslint-disable-next-line linebreak-style
 
-function opentab(tabname) {
- 
-  for(tablink of tablinks) {
-    tablink.classlist.remove('active-link');
+// eslint-disable-next-line no-unused-vars, linebreak-style
+function openTab(tabname) {
+  // eslint-disable-next-line no-restricted-syntax, no-const-assign, linebreak-style
+  for (tablinks of tablinks) {
+    // eslint-disable-next-line linebreak-style
+    tablinks.classlist.remove('active-link');
   }
+  // eslint-disable-next-line no-trailing-spaces
   
-  for(tabcontent of tabcontents) {
-    tabcontent.classlist.remove('active-tab');
+  // eslint-disable-next-line no-restricted-syntax, no-const-assign
+  for (tabcontents of tabcontents) {
+    tabcontents.classlist.remove('active-tab');
   }
+  // eslint-disable-next-line no-trailing-spaces
   
+  // eslint-disable-next-line no-restricted-globals
   event.currentTarget.classlist.add('active-link');
   document.getElementById.classlist.Add('active-tab');
 }
